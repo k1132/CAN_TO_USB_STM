cf319e3eb01c5a59d63565ee7483f477f3042317
ZubovIG <ZubovIG@kamaz.ru>
2017-10-27 12:36:07 +0300

    new

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.d
@@ -0,0 +1,89 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/GIT/STM32/CAN_TO_USB/Inc/stm32f4xx_hal_conf.h \
+ D:/GIT/STM32/CAN_TO_USB/Inc/main.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cm4.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_can.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/GIT/STM32/CAN_TO_USB/Inc/stm32f4xx_hal_conf.h:
+
+D:/GIT/STM32/CAN_TO_USB/Inc/main.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_can.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+D:/GIT/STM32/CAN_TO_USB/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
